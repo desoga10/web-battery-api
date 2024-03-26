e4f6903d5faf6a29a492f722891acbd96af2e7e5
desoga10 <done2stay@gmail.com>
2024-03-26 01:43:05 +0100

    Initial Commit"

---
+++ app.js
@@ -0,0 +1,31 @@
+function updateBatteryStatus(battery) {
+  document.getElementById('battery-level')
+    .textContent =
+    `${battery.level * 100}%`;
+
+
+  document.getElementById('charging-status')
+    .textContent =
+    `${battery.charging ? 'Yes' : 'No'}`;
+}
+
+
+
+
+
+
+navigator.getBattery().then(function (battery) {
+  // Battery Information
+  updateBatteryStatus(battery);
+
+  // Event Listeners for the Battery API
+  battery.addEventListener('chargingchange',
+    function () {
+      updateBatteryStatus(battery);
+    });
+
+  battery.addEventListener('chargingtimechange',
+    function () {
+      updateBatteryStatus(battery);
+    });
+});
